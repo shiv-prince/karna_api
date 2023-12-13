b686a3858eea40a5d31faf855f38cb89dde356fb
shivu-prince <shivuprince771@gmail.com>
2023-12-13 18:57:04 +0530

    get_data

---
+++ get_data.php
@@ -0,0 +1,16 @@
+<?php
+include("dbconnection.php");
+$con=debconnection();
+
+$query="SELECT * FROM `K_User-table` ";
+
+$exe=mysqli_query($con, $query);
+
+$arr=[];
+
+while($row = mysqli_fetch_array($exe)){
+    $arr[]=$row
+}
+
+print(json_encode($arr))
+?>
